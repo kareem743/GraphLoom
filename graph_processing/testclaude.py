01e5cbbd1f7dd8fa2b885b94e7f68f2e6fa8886c
Kareem  Jamal <85964485+kareem743@users.noreply.github.com>
2025-05-03 15:47:04 +0300

    Delete graph_processing directory

--- graph_processing/testclaude.py
+++
@@ -1,4 +0,0 @@
-api_key = "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"
-from anthropic import Anthropic
-client = Anthropic(api_key=api_key)
-client.messages.create(model='claude-3-7-sonnet-20250219')
